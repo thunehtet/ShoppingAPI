478c10295ecb329d7e4309735430d19359154133
thunehtet <130465751+thunehtet@users.noreply.github.com>
2024-09-30 14:38:46 +0800

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Entities;
+
+namespace Shopping.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext storeContext) 
+        {
+            _context = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get() 
+        {
+            return await _context.Products.ToListAsync();
+           // return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+        
+    }
+}
